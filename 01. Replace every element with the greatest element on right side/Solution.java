078299b10b87b69f9903e7d0fa8b5212534822f5
Mohan Krishna Battala <112927860+MohanKrishna-2003@users.noreply.github.com>
2023-08-30 21:04:26 +0530

    Update Solution.java

--- 01. Replace every element with the greatest element on right side/Solution.java
+++ 01. Replace every element with the greatest element on right side/Solution.java
@@ -1,1 +1,20 @@
-
+import java.util.*;
+public class Main 
+{
+    public static void main (String[] args) 
+    {
+        int arr[] = {16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+        int max = arr[n-1];  
+        arr[n-1] = -1;
+        for(int i=n-2; i>=0; i--)
+        {
+            int temp = arr[i];
+            arr[i] = max;
+            if(temp>max)
+                max = temp;
+        }
+        for(int i: arr)
+            System.out.print(i + " ");
+    }
+}
